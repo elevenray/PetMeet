1969bff115aab9a3d05f81e0b6aa391adf664776
Raymond Laurente <raymondlaurente@csu.fullerton.edu>
2020-03-18 10:23:14 -0700

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,7 @@
+<!-- footer -->
+<footer id="sticky-footer" class="py-3 bg-dark text-white-50 fixed-bottom" >
+  <div class="container text-center " >
+     <small>Developed By &copy;UACDOOM </small>
+    </div>
+</footer>
+<!-- end of footer -->
